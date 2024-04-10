ff700612f084597f036c17c2ed9e967b0d9d1ed8
Atla004 <andrese.g.v.13579@gmail.com>
2024-04-10 02:40:40 -0400

    Co-authored-by: Tomas Santana <Tomas-Santana@users.noreply.github.com>

--- src/Components/BillCard.tsx
+++ src/Components/BillCard.tsx
@@ -48,48 +48,32 @@ interface Result {
 }
 
 interface BillCardProps {
-  type: number;
+  type: "total" | "average";
   result: Result;
   filterDate?: string;  // Add a new prop for the filter date
 }
 
-export default function BillCard({ type, result, filterDate }: BillCardProps) {
-  if ((type === 3 || type === 4) && !filterDate) {
-    return <div>Error: filterDate is required when type is 3 or 4</div>;
-  }
+export default function BillCard({ type, result }: BillCardProps) {
 
-  //filtra data por date
-  const invoices = filterDate
-    ? result.invoices.filter(invoice => invoice.date === filterDate)
-    : result.invoices;
 
-  const totalSum = invoices.reduce((sum, invoice) => sum + invoice.total, 0);
-  const averageInvoice = invoices.reduce((sum, invoice) => sum + invoice.total, 0) / invoices.length;
+
+  const totalSum = result.day_total;
+  const averageInvoice = result.average_invoice;
 
 
 
   let cardContent, cardTitle, cardDescription;
 
   switch(type) {
-    case 1:
+    case "total":
       cardContent = "Total";
       cardTitle = ` $${totalSum.toFixed(2)}`;
       cardDescription = "suma de todas las facturas ";
       break;
-    case 3:
-      cardContent = "Total";
-      cardTitle = ` $${totalSum.toFixed(2)}`;
-      cardDescription = `suma de todas las facturas del ${filterDate}`;
-      break;
-    case 2:
+    case "average":
       cardContent = "average_invoice";
       cardTitle = ` $${averageInvoice.toFixed(2)}`;
       cardDescription = "promedio de monto por factura";
-      break;
-    case 4:
-      cardContent = "average_invoice";
-      cardTitle = ` $${averageInvoice.toFixed(2)}`;
-      cardDescription =   `promedio de monto por factura del ${filterDate}`;
       break;
   }
 
--- src/Views/Dashboard.tsx
+++ src/Views/Dashboard.tsx
@@ -11,52 +11,7 @@ type HelloResponse = {
     message: string;
 }
 
-const invoices = [
-    {
-      invoice: "INV001",
-      Banco: "BNC",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      Banco: "Banesco",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      Banco: "Banesco",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      Banco: "BNC",
-      totalAmount: "$430.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      Banco: "BNC",
-      totalAmount: "$120.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      Banco: "BNC",
-      totalAmount: "$100.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      Banco: "Venezuela",
-      totalAmount: "$200.00",
-      paymentMethod: "Credit Card",
-    },
-  ]
-
-const factura ={
+const facturaYDetalles ={
 "result": {
     "average_invoice": 54.63333333333333,
     "banks": [
@@ -137,38 +92,61 @@ const factura ={
 }
 }
 
+
 export default function Dashboard() {
     const [message, setMessage] = useState<HelloResponse>({message: ""});
     //const [isLoading, setIsLoading] = useState<boolean>(true);
     const [openCerrarCaja, setCerrarCaja] = useState(false);
-    const [filterDate, setFilterDate] = useState('2024-04-09');
-    console.log(filterDate);
+    const [filterDate, setFilterDate] = useState(new Date().toISOString().split('T')[0]); // fecha de la factura
+    const [closingStatement, setClosingStatement] = useState(null); // array de los datos de cierre de caja
+    const [maxDate, setMaxDate] = useState(filterDate);
+
+    useEffect(() => {
+      setMaxDate(filterDate);
+    }, []);
+
+
+
     useEffect(() => {
         const fetchData = async () => {
-            //setIsLoading(true);
-            const response = await fetch("http://127.0.0.1:5000/hello");
+            const body = {
+                date: filterDate
+            };
+            const response = await fetch("127.0.0.1:5000/getClosingStatement", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            });
             const data = await response.json();
-            setMessage(data);
+            console.log(data);
+            setClosingStatement(data.result);
             //setIsLoading(false);
-          
+            console.log(closingStatement);
+            //setFilterDate(closingStatement.date);
+
         }
         fetchData();
-    }, [])
+    }, [filterDate]);
+
+
 
     useKeyCombination(() => {
         setCerrarCaja(true);
       }, ["ctrl", "alt", "c"]);
 
     const cerrarCaja = () => {
-        console.log('El usuario hizo clic en el botón de acción');
+        console.log('La acción al cerrar caja');
     };
 
-
-    /* //SOLO CARGA CON EL BACKEND ABIERTO
+/*
+     //SOLO CARGA CON EL BACKEND ABIERTO
     if (isLoading) {
         return <p>Cargando...</p>;
     }
-    */
+*/
+
 
     return (
         <>
@@ -178,12 +156,11 @@ export default function Dashboard() {
                 {message.message && <p>{message.message}</p>}
 
                 <div className="content p-2 grid grid-cols-1 md:grid-cols-3 md:grid-rows-2 gap-4">
-                <BillDate setFilterDate={setFilterDate} filterDate={filterDate}></BillDate>
+                <BillDate setFilterDate={setFilterDate} filterDate={filterDate} maxDate = {maxDate}></BillDate>
 
                     <div className="col-span-1 md:row-span-1">
-                        <BillCard type = {1} result={factura.result}></BillCard>
-                        <BillCard type = {2} result={factura.result}></BillCard>
-                        <BillCard type = {3} result={factura.result} filterDate={filterDate}></BillCard>
+                        <BillCard type="total" result={facturaYDetalles.result}  />
+                        <BillCard type="average" result={facturaYDetalles.result}  />
                     </div>
                     <div className="col-span-2 md:col-span-2 md:row-span-2 flex items-center justify-center">
                         <ActionAlertProg
@@ -197,12 +174,13 @@ export default function Dashboard() {
                         />
                     </div>
                     <div className="col-span-2 md:col-span-2 md:row-span-2 flex items-center justify-center">
-                        <BarChartA type={1} invoices={invoices}></BarChartA>
-                        <BarChartA type={2} invoices={invoices}></BarChartA>
-                        <BarChartA type={3} invoices={invoices}></BarChartA>
+                        <BarChartA type="total" invoices={facturaYDetalles.result.banks} />
+                        <BarChartA type="total" invoices={facturaYDetalles.result.methods} />
+                        <BarChartA type="total" invoices={facturaYDetalles.result.products} />
+                        <BarChartA type="sold" invoices={facturaYDetalles.result.products} />
+
                     </div>
                     <div className="col-span-2 md:row-span-2">
-                        <TableCard invoices={invoices}></TableCard>
                     </div>
                 </div>
             </div>
